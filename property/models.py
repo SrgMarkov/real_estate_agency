ea59959b0750a5adf02e604602ed85b8cd55ee13
SrgMarkov <107784915+SrgMarkov@users.noreply.github.com>
2023-07-03 21:29:04 +0400

    Change liked_by Related_name

--- property/models.py
+++ property/models.py
@@ -47,7 +47,7 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
-    liked_by = models.ManyToManyField(User, verbose_name='Понравилась', related_name="liked_posts", blank=True)
+    liked_by = models.ManyToManyField(User, verbose_name='Понравилась', related_name="liked_flats", blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
